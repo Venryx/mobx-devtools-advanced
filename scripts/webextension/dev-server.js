08232574408b56fe10d4066ad605fd444451a036
Venryx <venryx@gmail.com>
2019-11-14 02:46:44 -0800

    * FINALLY FIXED that annoying issue of webpack-dev-server refreshing the main page whenever the devtools needed refreshing (due to code changes). I know sometimes a full page refresh is necessary (due to the injected content scripts), but most of the time it isn't, and the slowdown from loading the main page every time outweighs the small convenience of having it occasionally save a keystroke or two. * Added start:chrome:debug-dev-server command. (it does what it sounds like)

--- scripts/webextension/dev-server.js
+++ scripts/webextension/dev-server.js
@@ -2,6 +2,7 @@ const WebpackDevServer = require('webpack-dev-server');
 const webpack = require('webpack');
 const config = require('../../src/shells/webextension/webpack.config.js');
 const path = require('path');
+const StringReplacerPlugin = require('webpack-string-replacer');
 
 const { TARGET_BROWSER } = process.env;
 const rootDir = path.join(__dirname, '../../');
@@ -23,6 +24,64 @@ const HR_PORT = 5000;
 
 // config.plugins = [new webpack.HotModuleReplacementPlugin()].concat(config.plugins || []);
 
+// maybe temp; disable warnings for webpack output (so webpack doesn't get flooded with them)
+config.plugins.push({
+  apply(compiler) {
+    compiler.plugin('emit', (compilation, cb) => {
+      compilation.warnings = [];
+      cb();
+    });
+  },
+});
+
+// hacky fix for webpack-dev-server causing the (non-extension) page to reload on any changes to dev-tools source!
+// (normally, the changes are UI only, so no need to reload the content-script or page)
+// config.plugins.splice(0, 0, new StringReplacerPlugin({ // place at start, to fix bug
+/* config.plugins.push(new StringReplacerPlugin({
+  // logFileMatches: true,
+  // logFileMatchContents: true,
+  logAroundPatternMatches: 200,
+  rules: [{
+    // stage: "readFromDisk",
+    // bundleInclude: {entryPath: /injectGlobalHook.js$/},
+    chunkInclude: { name: 'injectGlobalHook' },
+    chunkMatchCount: 1,
+    // include: /injectGlobalHook.js$/,
+    // fileInclude: /webpack-dev-server\//,
+    // include: /webpack-dev-server\/.+\/index.js/,
+    // include: /webpack-dev-server\/.+\/reloadApp.js$/,
+    // fileMatchCount: {min: 1},
+    replacements: [
+      {
+        // pattern: 'rootWindow.location.reload();',
+        pattern: '.location.reload()',
+        // pattern: /\.location\.reload\(\)/g,
+        // patternMatchCount: {min: 1},
+        patternMatchCount: 4,
+        // replacement: ';//.location.reload()',
+        replacement: ';//.location.reload()',
+        // replacement: ';//cation.reload()', // must match length of pattern, due to bug
+      },
+    ],
+  }],
+})); */
+config.plugins.push(new StringReplacerPlugin({
+  rules: [{
+    applyStage: 'optimizeChunkAssets',
+    outputFileInclude: [
+      'injectGlobalHook.js',
+      'backend.js',
+      'contentScript.js',
+    ],
+    replacements: [
+      {
+        pattern: '.location.reload()',
+        replacement: ';//.location...reload()',
+      },
+    ],
+  }],
+}));
+
 const compiler = webpack(config);
 
 const server = new WebpackDevServer(compiler, {
@@ -36,7 +95,23 @@ const server = new WebpackDevServer(compiler, {
   hot: false,
   liveReload: true, // even though only live-reload is enabled, it somehow ends up refreshing the (non-extension) page itself as well!
   contentBase: path.join(rootDir, 'lib', TARGET_BROWSER),
+  /* contentBase: [
+    path.join(rootDir, 'lib', TARGET_BROWSER, "panel.js"),
+    path.join(rootDir, 'lib', TARGET_BROWSER, "window.js"),
+  ], */
   headers: { 'Access-Control-Allow-Origin': '*' },
+  /* proxy: {
+    /* {
+      context: ['injectGlobalHook.js'],
+    }, *#/
+    '/injectGlobalHook.js': {
+      target: 'http://localhost:[port]/',
+      pathRewrite: { '^/injectGlobalHook.js': '' },
+    },
+  }, */
+  stats: {
+    warnings: false,
+  },
 });
 
 server.listen(HR_PORT);
